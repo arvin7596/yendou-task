517895b32f815b053997bf21138f8d8019d11d9f
Arvin Ghaffari <arvinghaffari75@gmail.com>
2024-09-11 23:40:27 +0200

    style : install Inter font

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import '@fontsource/inter/400.css'; // Regular weight
+import '@fontsource/inter/700.css'; // Bold weight
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
